146f927cd8ed442556a9902c5260f7d9fa82620f
Geiger Kevin <kevingeiger@smartx.eu>
2022-12-01 22:29:11 +0100

    day 1 changed to module

---
+++ day1.ex
@@ -0,0 +1,23 @@
+defmodule Day1 do
+  def calories() do
+    File.read!("day1.txt")
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(&(String.split(&1, "\n", trim: true)))
+    |> Enum.map(fn elf -> Enum.map(elf, &String.to_integer/1) end)
+    |> Enum.map(&Enum.sum/1)
+end
+
+  # Part 1
+  def part1() do
+    calories()
+    |> Enum.max()
+  end
+
+  # Part 2
+  def part2() do
+    calories()
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.sum()
+  end
+end
