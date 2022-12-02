4f7cc8c17b1ee51d2cb8d016f79c17e8d693c693
Geiger Kevin <kevingeiger@smartx.eu>
2022-12-02 14:28:15 +0100

    day2 improved, gitignore input file

--- day2.ex
+++ day2.ex
@@ -1,44 +1,22 @@
 defmodule Day2 do
   def input() do
-    File.stream!("day2.txt")
+    File.stream!("2.txt")
     |> Stream.map(fn
-      "A " <> guess -> {:A, Enum.at(to_charlist(guess) ++ [0], 0) - 87}
-      "B " <> guess -> {:B, Enum.at(to_charlist(guess) ++ [0], 0) - 87}
-      "C " <> guess -> {:C, Enum.at(to_charlist(guess) ++ [0], 0) - 87}
+      input -> {Enum.at(to_charlist(String.at(input, 0)) ++ [0], 0) - 65, Enum.at(to_charlist(String.at(input, 2)) ++ [0], 0) - 88}
     end)
   end
 
   def part1() do
     input()
     |> Enum.reduce(0, fn
-      {:A, guess = 3}, sum -> sum + guess + 0
-      {:A, guess = 1}, sum -> sum + guess + 3
-      {:A, guess = 2}, sum -> sum + guess + 6
-
-      {:B, guess = 1}, sum -> sum + guess + 0
-      {:B, guess = 2}, sum -> sum + guess + 3
-      {:B, guess = 3}, sum -> sum + guess + 6
-
-      {:C, guess = 2}, sum -> sum + guess + 0
-      {:C, guess = 3}, sum -> sum + guess + 3
-      {:C, guess = 1}, sum -> sum + guess + 6
+      {elf, me}, sum -> sum + me+1 + rem(elf-me+1, 3)*3
     end)
   end
 
   def part2() do
     input()
     |> Enum.reduce(0, fn
-      {:A, 1}, sum -> sum + 3 + 0
-      {:A, 2}, sum -> sum + 1 + 3
-      {:A, 3}, sum -> sum + 2 + 6
-
-      {:B, 1}, sum -> sum + 1 + 0
-      {:B, 2}, sum -> sum + 2 + 3
-      {:B, 3}, sum -> sum + 3 + 6
-
-      {:C, 1}, sum -> sum + 2 + 0
-      {:C, 2}, sum -> sum + 3 + 3
-      {:C, 3}, sum -> sum + 1 + 6
+      {elf, outcome}, sum -> sum + rem(elf+outcome-1, 3)+1 + outcome*3
     end)
   end
 end
